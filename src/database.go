4723c01a5157442303c3ef397be05b8bdca10435
jmurrah <jacobhmurrah@gmail.com>
2024-06-19 22:35:39 -0500

    Insert and Update username and email in database

--- src/database.go
+++ src/database.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -8,12 +9,16 @@ import (
 	"github.com/supabase-community/supabase-go"
 )
 
-type Problem struct {
+type LeetCodeProblem struct {
 	TitleSlug      string
 	Difficulty     string
 	Tags           []string
 	CompletedDates []time.Time
 	RepeatDate     time.Time
+}
+
+type EmailListResponse struct {
+	EmailList []string `json:"email_list"`
 }
 
 func create_supabase_client() *supabase.Client {
@@ -26,45 +31,34 @@ func create_supabase_client() *supabase.Client {
 	return client
 }
 
-func add_username_and_email_to_database(leetcode_username string, email string) {
+func add_username_and_email_to_database(leetcode_username string, new_email string) {
 	client := create_supabase_client()
 	table := os.Getenv("SUPABASE_TABLE")
 
 	found_username, _, _ := client.From(table).Select("username", "", false).Eq("username", leetcode_username).Execute()
 
 	if string(found_username) == "[]" {
-		// User not in db = ADD USERNAME to database
 		data := map[string]interface{}{
 			"username":   leetcode_username,
-			"email_list": []string{email},
-			"problems":   []Problem{},
+			"email_list": []string{new_email},
+			"problems":   []LeetCodeProblem{},
 		}
 		client.From(table).Insert(data, false, "Failure", "Success", "1").Execute()
 	} else {
-		// User IN db, update the email list
-		// data := map[string]interface{}{
-		// 	"email": []string{email},
-		// }
+		var old_emails []EmailListResponse
+		var updatedEmails []string
+
 		response, _, _ := client.From(table).Select("email_list", "", false).Eq("username", leetcode_username).Execute()
+		json.Unmarshal(response, &old_emails)
 
-		// Assuming the response is a single row
-		// row := response.(map[string]interface{})
-		// currentEmails := row["email"].([]string) // Assuming "email" is a slice of strings
+		for _, old_email := range old_emails {
+			updatedEmails = append(updatedEmails, old_email.EmailList...)
+		}
+		updatedEmails = append(updatedEmails, new_email)
 
-		// fmt.Println(string(row))
-		fmt.Println(string(response))
-		// // Append to the list
-		// newEmail := "newEmail@example.com" // Replace with the actual email you want to append
-		// updatedEmails := append(currentEmails, newEmail)
-
-		// // Update the data
-		// data := map[string]interface{}{
-		// 	"email": updatedEmails,
-		// }
-		// _, err = client.From(table).Update(data).Eq("username", leetcode_username).Execute()
-		// if err != nil {
-		// 	fmt.Println("cannot perform update operation", err)
-		// 	return
-		// }
+		data := map[string]interface{}{
+			"email_list": updatedEmails,
+		}
+		client.From(table).Update(data, "", "").Eq("username", leetcode_username).Execute()
 	}
 }
