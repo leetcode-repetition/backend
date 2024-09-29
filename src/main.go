829513eeceb63177d522b97f5e04399d9b15e235
jmurrah <jacobhmurrah@gmail.com>
2024-09-28 21:00:58 -0500

    first prototype of working version

--- src/main.go
+++ src/main.go
@@ -20,7 +20,7 @@ type LeetCodeProblem struct {
 func enableCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == "OPTIONS" {
