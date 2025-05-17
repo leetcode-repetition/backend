eed328ed91198834386918d62b0807063b1913e5
jmurrah <jacobhmurrah@gmail.com>
2025-05-17 15:38:54 -0500

    able to pass in the correct variables to the lambda

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 Hello world!
+
+local deployment
 ```
 export DOCKER_BUILDKIT=1
 sam build --use-container --cached --parallel
 sam local start-api --env-vars env.json
+```
+
+updating lambda (wsl/zsh)
+```
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bootstrap main.go
+zip function.zip bootstrap main.go
+aws lambda update-function-code --function-name my:lambda:arn --zip-file fileb://function.zip
 ```
--- lambdas/create-key/main.go
+++ lambdas/create-key/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +19,17 @@ import (
 
 var apiGatewayClient *apigateway.Client
 
+type LeetCodeUserStatus struct {
+	UserId   json.Number `json:"userId"`
+	Username string      `json:"username"`
+}
+
+type LeetCodeGraphQLResponse struct {
+	Data struct {
+		UserStatus LeetCodeUserStatus `json:"userStatus"`
+	} `json:"data"`
+}
+
 func init() {
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
@@ -26,35 +39,74 @@ func init() {
 	apiGatewayClient = apigateway.NewFromConfig(cfg)
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	origin := request.Headers["Origin"]
-	if origin == "" {
-		origin = request.Headers["origin"]
-	}
-	if origin == "" {
-		origin = "null"
-	}
-	cors := map[string]string{
-		"Access-Control-Allow-Origin":      origin,
-		"Access-Control-Allow-Credentials": "true",
-		"Access-Control-Allow-Methods":     "OPTIONS,GET,POST,DELETE",
-		"Access-Control-Allow-Headers":     "Content-Type,x-pkce-verifier,x-auth-code,x-csrf-token,x-api-key",
-		"Vary":                             "Origin",
+func fetchLeetCodeUserInfo(csrfToken, leetcodeSession string) (*LeetCodeUserStatus, error) {
+	query := map[string]interface{}{
+		"query": `
+            query {
+                userStatus {
+                    userId
+                    username
+                }
+            }
+        `,
 	}
 
-	if request.HTTPMethod == http.MethodOptions {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 204,
-			Headers:    cors,
-		}, nil
+	jsonQuery, err := json.Marshal(query)
+	if err != nil {
+		return nil, err
 	}
+
+	req, err := http.NewRequest("POST", "https://leetcode.com/graphql", bytes.NewBuffer(jsonQuery))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Csrf-Token", csrfToken)
+	req.Header.Set("X-Leetcode-Session", leetcodeSession)
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("LeetCode API response: %s", string(body))
+
+	var graphQLResponse LeetCodeGraphQLResponse
+	if err := json.Unmarshal(body, &graphQLResponse); err != nil {
+		return nil, err
+	}
+
+	return &graphQLResponse.Data.UserStatus, nil
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	cors := map[string]string{
+		"Access-Control-Allow-Origin":      request.Headers["origin"],
+		"Access-Control-Allow-Credentials": "true",
+		"Access-Control-Allow-Methods":     "POST",
+		"Access-Control-Allow-Headers":     "Content-Type,X-Api-Key,X-Pkce-Verifier,X-Auth-Code,X-Csrf-Token,X-Leetcode-Session",
+		"Content-Type":                     "application/json",
+	}
+	log.Printf("Request headers: %+v", request.Headers)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	log.Printf("Query parameters: %+v", request.QueryStringParameters)
-	userId := request.QueryStringParameters["userId"]
-	log.Printf("UserId: %s", userId)
+	csrfToken := request.Headers["x-csrf-token"]
+	leetcodeSession := request.Headers["x-leetcode-session"]
+
+	userInfo, _ := fetchLeetCodeUserInfo(csrfToken, leetcodeSession)
+	userId := userInfo.UserId.String()
+
+	log.Printf("%+v", userInfo)
 
 	keyName := "LRE_" + userId + "_" + time.Now().Format("20060102150405")
 	log.Printf("Creating API key with name: %s", keyName)
